3b49399fa4157f619f5a598c3b1dd5a7163ffb7d
ryaan <ryaanamank91@gmail.com>
2024-08-28 19:25:47 +0200

    Day24: Add footers

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fpdf import FPDF  # library to create pdf from python
 
 # to create the pdf object
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 # create the dataframe
 df = pd.read_csv('topics_day24.csv')
@@ -17,9 +18,23 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)  # sets the config of page and the content
     pdf.line(x1=10, y1=21, x2=200, y2=21)  # To add an underline below the topics
 
+    pdf.ln(265)  # to add the break line
+
+    # set the footer for the master page
+    pdf.set_font(family="Times", style="I", size=10)  # sets the font of the footer
+    pdf.set_text_color(100, 100, 100)  # to set the text color
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R", )  # sets the config of footer and the content
+
     # to iterate inside the loop of the page and add more pages for the same topic
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+
+        pdf.ln(275)  # to add the break line
+
+        # set the footer for the other pages
+        pdf.set_font(family="Times", style="I", size=10)  # sets the font of the footer
+        pdf.set_text_color(180, 180, 180)  # to set the text color
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R", )  # sets the config of footer and the content
 
 # output the pdf file on the disk
 pdf.output("output.pdf")
